79ca2c727c18af9e93ecdfccb525acd8b1892c04
Juan Carlos <juancagb.17@gmail.com>
2018-02-19 08:23:03 -0500

    get references

---
+++ piedra-papel-tijeras/app.js
@@ -0,0 +1,9 @@
+const imgPlayerChoice = document.getElementById('playerChoice');
+const imgComputerChoice = document.getElementById('computerChoice');
+
+const pResult = document.getElementById('result');
+const pScore = document.getElementById('score');
+
+const buttons = document.querySelectorAll('button');
+
+
