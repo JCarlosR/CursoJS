90c985f4f20591afc58b367570c64fe14b8a7113
Juan Carlos <juancagb.17@gmail.com>
2018-03-11 21:33:36 -0500

    Add food generation and colors

--- snake/snake.js
+++ snake/snake.js
@@ -3,10 +3,8 @@ const context = myCanvas.getContext('2d');
 
 const SIZE = 20;
 
-const head = {
-	x: 0,
-	y: 0
-};
+const head = { x: 0, y: 0 };
+let food = null; // x: y:
 
 let dx = 0;
 let dy = 0;
@@ -19,17 +17,45 @@ function main() {
 }
 
 function update() {
+	// actualizar las coords de la cabeza de la serpiente
 	head.x += dx;
 	head.y += dy;
+
+	// detectar si la serpiente ha consumido el alimento
+	if (food && head.x === food.x && head.y === food.y) {
+		food = null;
+		// aumentar el tamaño de la serpiente
+	}
+	
+
+	// generar el alimente en caso que no exista
+	if (!food) {
+		food = { x: getRandomX(), y: getRandomY() };
+	}
+}
+
+function getRandomX() {
+	// 0, 20, 40, ..., 380
+	// 0, 1, 2, ..., 19       x20
+	return 20 * parseInt(Math.random() * 20);
+}
+
+function getRandomY() {
+	// 0, 20, 40, ..., 440
+	// 0, 1, 2, ..., 22
+	return 20 * parseInt(Math.random() * 23);	
 }
 
 function draw() {
-	context.clearRect(0, 0, myCanvas.width, myCanvas.height);
-	drawObject(head);
+	context.fillStyle = 'black';
+	context.fillRect(0, 0, myCanvas.width, myCanvas.height);
+	drawObject(head, 'lime');
+	drawObject(food, 'white');
 }
 
 
-function drawObject(obj) {
+function drawObject(obj, color) {
+	context.fillStyle = color;
 	context.fillRect(obj.x, obj.y, SIZE, SIZE);
 }
 
